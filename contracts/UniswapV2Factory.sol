1d9b760f96a645630d2fe5e200a51429fe0690e2
river7god <116942912+river7god@users.noreply.github.com>
2022-11-04 17:20:01 +0800

    Update UniswapV2Factory.sol

--- contracts/UniswapV2Factory.sol
+++ contracts/UniswapV2Factory.sol
@@ -4,8 +4,8 @@ import './interfaces/IUniswapV2Factory.sol';
 import './UniswapV2Pair.sol';
 
 contract UniswapV2Factory is IUniswapV2Factory {
-    address public feeTo;
-    address public feeToSetter;
+    address public feeTo;    // 收税的地址
+    address public feeToSetter;  // 收税的地址设置者
 
     mapping(address => mapping(address => address)) public getPair;
     address[] public allPairs;
@@ -23,11 +23,19 @@ contract UniswapV2Factory is IUniswapV2Factory {
 
     function createPair(address tokenA, address tokenB) external returns (address pair) {
         require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');
+        // tokenA tokenB 进行大小排序
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
+        // 判断配对合约是否存在
         require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
+        
+        // 获得creationCode 编译后代码
         bytes memory bytecode = type(UniswapV2Pair).creationCode;
+        
+       // token0 和token1打包后创建哈希获得 盐，给create2方法使用 
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        
+        // 内联汇编, solidity没有的方法可以使用内联汇编来实现
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
